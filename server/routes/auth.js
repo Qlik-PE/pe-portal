b8ee3a5fd4d6e5ff68031aba9b539622da6848cb
Nick Webster <nick.webster@qlikview.com>
2015-08-12 14:35:51 +0200

    Change to reset password email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -33,7 +33,7 @@ router.post("/forgot", Auth.generateResetToken, function(req, res){
     subject: "PV Portal Password Reset",
     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-         'http://' + req.headers.host + '/#resetpassword/' + encodeURIComponent(req.token) + '\n\n' +
+         'http://' + req.headers.host + '/#resetpassword?token=' + encodeURIComponent(req.token) + '\n\n' +
          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
   };
   smtpTransport.sendMail(options, function(err){
